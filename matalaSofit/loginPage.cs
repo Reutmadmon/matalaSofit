5da26b2fbaf56e5e77ed2c544f4040183865bb89
hp <hp@HP-REUT>
2025-06-10 17:25:15 +0300

    Add project files.

---
+++ matalaSofit/loginPage.cs
@@ -0,0 +1,15 @@
+namespace matalaSofit
+{
+    public partial class loginPage : Form
+    {
+        public loginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            
+        }
+    }
+}
