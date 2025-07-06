71c5b858c59229a38051a1d6087fa1e0d4830ef2
ShaniHirsch <shanih2308@gmail.com>
2025-07-06 12:30:02 +0300

    Add files via upload
    backend

---
+++ matala-sofit/Client.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matala_sofit
+{
+    public class Client : Person
+    {
+        List<Book> booksBorrows { get; set; } = new List<Book>();
+        List<Book> booksHistory { get; set; } = new List<Book>();
+        //private double debt{get;set; } to think about how
+       
+        public Client(string name, string lastName, int id,string password, int age, int phoneNum,
+                       string email, string photo)
+           : base(name, lastName, id, password, age, phoneNum, email, photo)
+        {
+
+        }
+        public void BorrowBook(Book book)
+        {
+            if (book != null && !booksBorrows.Contains(book))
+            {
+                booksBorrows.Add(book);
+            }
+            else
+            {
+                throw new Exception("Book is already borrowed or does not exist.");
+            }
+        }
+        public void ReturnBook(Book book)
+        {
+            if (book != null && booksBorrows.Contains(book))
+                booksBorrows.Remove(book);
+            if (!booksHistory.Contains(book))
+            {
+                booksHistory.Add(book);
+            }
+    }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Client other)
+                return base.Equals(other);
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string borrows = booksBorrows != null && booksBorrows.Count > 0
+                ? string.Join(", ", booksBorrows)
+                : "None";
+
+            string history = booksHistory != null && booksHistory.Count > 0
+                ? string.Join(", ", booksHistory)
+                : "None";
+
+            return base.ToString() + "\n" +
+                    $"Books Borrowed: {borrows}\n" +
+                    $"Books History: {history}";
+        }
+    }
+}
---
+++ matala-sofit/Genre.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
+using System.Xml.Linq;
+
+namespace matala_sofit
+{
+    public class Genre
+    {
+        internal string name { get; set; }
+        internal string ageRestriction { get; set; }
+        internal string shortDescription { get; set; }
+        internal Book[] books { get; set; }
+        public Genre(string name, string ageRestriction, string description, Book[] books)
+        {
+            this.name = name;
+            this.ageRestriction = ageRestriction;
+            this.shortDescription = description;
+            this.books = books;
+        }
+        
+        public override string ToString()
+        {
+            string bookTitles = books != null && books.Length > 0
+                ? string.Join(", ", books.Select(b => b.title))
+                : "No books";
+
+            return $"Genre: {name}\n" +
+                   $"Age Restriction: {ageRestriction}\n" +
+                   $"Description: {shortDescription}\n" +
+                   $"Books: {bookTitles}";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Genre other)
+                return this.name == other.name;
+            return false;
+        }
+
+    }
+}
